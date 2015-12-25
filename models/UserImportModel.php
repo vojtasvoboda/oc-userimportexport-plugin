ec01caf3d59057b84a2bb985456064edf1074d14
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2015-12-25 12:03:02 +0100

    Import users avatar

--- models/UserImportModel.php
+++ models/UserImportModel.php
@@ -1,26 +1,54 @@
 <?php namespace VojtaSvoboda\UserImportExport\Models;
 
 use Backend\Models\ImportModel;
+use Cms\Classes\MediaLibrary;
+use System\Models\File;
+use RainLab\Location\Models\State;
 use RainLab\User\Models\User;
 
 class UserImportModel extends ImportModel
 {
     public $rules = [];
 
+    public $imageStoragePath = '/users';
+
+    public $imageVisibilityPublic = true;
+
     public function importData($results, $sessionKey = null)
     {
         foreach ($results as $row => $data)
         {
-            $username = $data['username'];
             $data += [
-                'password' => $username,
-                'password_confirmation' => $username,
-                'is_activated' => true
+                'is_activated' => true,
+
+                /* prepare for future usage */
+                // 'state_id' => $this->getStateId($data['state']),
+                // 'country_id' => 14
             ];
+
+            if ( empty($data['username']) ) {
+                $data['username'] = $data['email'];
+            }
+
+            if ( empty($data['password']) ) {
+                $data['password'] = $data['username'];
+            }
+
+            if ( empty($data['password_confirmation']) ) {
+                $data['password_confirmation'] = $data['password'];
+            }
 
             try {
                 $user = new User();
                 $user->fill($data);
+
+                // try to find avatar
+                $avatar = $this->findAvatar($data['username']);
+                if ( $avatar ) {
+                    $user->avatar = $avatar;
+                }
+
+                // save user
                 $user->save();
 
                 // activate user
@@ -33,4 +61,42 @@ class UserImportModel extends ImportModel
             }
         }
     }
+
+    /**
+     * @param $username
+     *
+     * @return string|null
+     */
+    private function findAvatar($username)
+    {
+        $library = MediaLibrary::instance();
+        $files = $library->listFolderContents($this->imageStoragePath, 'title', 'image');
+
+        foreach($files as $file) {
+            $pathinfo = pathinfo($file->publicUrl);
+            if ( $pathinfo['filename'] == $username ) {
+                $file = new File();
+                $file->is_public = $this->imageVisibilityPublic;
+                $file->fromFile(base_path() . $pathinfo['dirname'] . '/' . $pathinfo['basename']);
+
+                return $file;
+            }
+        }
+    }
+
+    /**
+     * Get state by ident
+     *
+     * @param $stateIdent
+     *
+     * @return null
+     */
+    private function getStateId($stateIdent)
+    {
+        $state = State::where('code', $stateIdent)->first();
+        if ( !$state ) return null;
+
+        return $state->id;
+    }
+
 }
