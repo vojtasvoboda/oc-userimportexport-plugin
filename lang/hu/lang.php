21f924f00410b2c2b79a22dc8db1bc7d22c4d6ea
Szabó Gergő <gergo85@gmail.com>
2016-12-19 09:25:59 +0100

    Add Hungarian translate

---
+++ lang/hu/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Felhasználók importálása/exportálása',
+        'description' => 'Provide import and export users'
+    ],
+    'label' => [
+        'import' => 'Importálás',
+        'export' => 'Exportálás',
+        'permission' => 'Importálás és exportálás'
+    ]
+];
